9dec447c7019cb911254ae61565614b51fae67ff
Owaki <30595535+owken64@users.noreply.github.com>
2020-09-17 22:38:48 +0900

    Add methods
    add(+) and multiplication(*)

--- Number.scala
+++ Number.scala
@@ -2,4 +2,13 @@ package com.yamanogusha.gungnir.base
 
 abstract sealed class Number(classname: String) extends Object(classname)
 
-class Integer(val value: Int) extends Number("Integer")
+class Integer(val value: Int) extends Number("Integer") {
+  def + (that: Integer): Integer = {
+    new Integer( this.value + that.value )
+  }
+  
+  def * (that: Integer): Integer = {
+    new Integer( this.value * that.value )
+  }
+}
+

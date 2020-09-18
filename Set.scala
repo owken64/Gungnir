619d361bcca6fba34ca4651377a4a592e31cc23b
Owaki <30595535+owken64@users.noreply.github.com>
2020-09-18 23:11:03 +0900

    Add a class of set
    Add finite set class

--- Set.scala
+++ Set.scala
@@ -4,6 +4,11 @@ sealed abstract class Set extends Object("Set") {
   def contains(obj: Object): Boolean
 }
 
+class finiteSet(list: List[Object]) {
+  val value = scala.collection.immutable.Set[Object](list)
+  def contains(obj: Object): Boolean = { value.contains(obj) }
+}
+
 class SetWithRequires(val requires: List[Object => Boolean] ) extends Set {
   def contains(obj: Object): Boolean = { requires.forall( P => (P(obj) == true) ) }
 }
